2552022fe715fa840049d39ce557f7fede3211f8
RoderickMS <Roderickxams@gmail.com>
2026-08-25 15:44:52 -0500

    botones

--- core/views.py
+++ core/views.py
@@ -1,19 +1,28 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 
 # Create your views here.
-from django.http import HttpResponse
-
 
 def inicio(request):
     return HttpResponse(
         "<h1>Desarrollo de Software VIII</h1>"
         "<p>Mi primer proyecto Django está funcionando correctamente.</p>"
+        "<a href='/estudiantes/'>Estudiantes</a><br>"
+        "<a href='/acerca/'>Acerca del proyecto</a>"
     )
+
 
 def acerca(request):
     return HttpResponse(
         "<h1>Acerca del proyecto</h1>"
         "<p>Proyecto desarrollado para Desarrollo de Software VIII.</p>"
         "<p>Grupo: 2GS131</p>"
+    )
+
+
+def estudiantes(request):
+    return HttpResponse(
+        "<h1>Estudiantes</h1>"
+        "<p>Lista de estudiantes del grupo 2GS131.</p>"
     )
